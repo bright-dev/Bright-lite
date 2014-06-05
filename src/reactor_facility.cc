9e1eccb94fa68426d876756d6a6ab3f56f3e1ba9
Anthony Scopatz <scopatz@gmail.com>
2014-06-05 17:36:53 -0500

    added state variables

--- src/reactor_facility.cc
+++ src/reactor_facility.cc
@@ -2,22 +2,35 @@
 
 namespace reactor {
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ReactorFacility::ReactorFacility(cyclus::Context* ctx)
     : cyclus::Facility(ctx) {};
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 std::string ReactorFacility::str() {
   return Facility::str();
 }
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void ReactorFacility::Tick() {}
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void ReactorFacility::Tock() {}
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+std::set<cyclus::RequestPortfolio<cyclus::Material>::Ptr> ReactorFacility::GetMatlRequests() {
+  using cyclus::RequestPortfolio;
+  using cyclus::Material;
+  using cyclus::CapacityConstraint;
+
+  Material::Ptr target = Material::CreateUntracked(core_mass/batches, );
+  CapacityConstraint<Material> cc(1.0);
+
+  RequestPortfolio<Material>::Ptr port(new RequestPortfolio<Material>());
+  port->AddRequest(target, this, in_commod);
+  port->AddConstraint(cc);
+
+  std::set<RequestPortfolio<Material>::Ptr> ports();
+  ports.insert(port);
+  return ports;
+
+}
+
 extern "C" cyclus::Agent* ConstructReactorFacility(cyclus::Context* ctx) {
   return new ReactorFacility(ctx);
 }
--- src/reactor_facility.h
+++ src/reactor_facility.h
@@ -59,7 +59,52 @@ class ReactorFacility : public cyclus::Facility  {
   /// @param time the time of the tock
   virtual void Tock();
 
-  // and away we go!
+  /// This facility has one output commodity and one input commodity
+  #pragma cyclus var {"tooltip": "input commodity", \
+                      "doc": "commodity that the brightlite reactor consumes"}
+  std::string in_commod;
+
+  #pragma cyclus var {"tooltip": "output commodity", \
+                      "doc": "commodity that the brightlite supplies"}
+  std::string out_commod;
+
+  #pragma cyclus var {"tooltip": "reactor libraries to load", \
+                      "doc": "the reactor's burnup & criticality behavior to use"}
+  std::vector<std::string> libraries;
+
+  #pragma cyclus var {"tooltip": "number of batches", \
+                      "default": 3}
+  int batches;
+
+  #pragma cyclus var {"tooltip": "Non-leakage probability", \
+                      "doc": "Varies from 0 - 1.", \
+                      "default": 0.98}
+  double nonleakage;
+
+  #pragma cyclus var {"default": 1e299, \
+                      "tooltip": "reactor maximum inventory size", \
+                      "doc": "total maximum inventory size of the reactor"}
+  double max_inv_size;
+
+  #pragma cyclus var {"tooltip": "Target burnup", \
+                      "units": "MWd/kgIHM"}
+  double target_burnup;
+
+  #pragma cyclus var {"default": 1000.0, \
+                      "units": "MWe", \
+                      "tooltip": "Electrical production."}
+  double generated_power;
+
+  #pragma cyclus var {"units": "kgIHM", \
+                      "tooltip": "Total mass of the core."}
+  double core_mass;
+
+  #pragma cyclus var {"default": 0.33, \
+                      "tooltip": "Thermal to electric conversion rate."}
+  double efficiency;
+
+  #pragma cyclus var {"capacity": "max_inv_size"}
+  cyclus::toolkit::ResourceBuff inventory;
 };
 
 }  // namespace reactor
