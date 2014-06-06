9e0a42807f0164e85f047e3b52b55cf7c4073405
Anthony Scopatz <scopatz@gmail.com>
2014-06-06 10:58:35 -0500

    It lives

--- src/reactor_facility.cc
+++ src/reactor_facility.cc
@@ -16,19 +16,22 @@ void ReactorFacility::Tock() {}
 std::set<cyclus::RequestPortfolio<cyclus::Material>::Ptr> ReactorFacility::GetMatlRequests() {
   using cyclus::RequestPortfolio;
   using cyclus::Material;
+  using cyclus::Composition;
+  using cyclus::CompMap;
   using cyclus::CapacityConstraint;
 
-  Material::Ptr target = Material::CreateUntracked(core_mass/batches, );
+  CompMap cm;
+  Material::Ptr target = Material::CreateUntracked(core_mass/batches, 
+                          Composition::CreateFromAtom(cm));
   CapacityConstraint<Material> cc(1.0);
 
   RequestPortfolio<Material>::Ptr port(new RequestPortfolio<Material>());
   port->AddRequest(target, this, in_commod);
   port->AddConstraint(cc);
 
-  std::set<RequestPortfolio<Material>::Ptr> ports();
+  std::set<RequestPortfolio<Material>::Ptr> ports;
   ports.insert(port);
   return ports;
-
 }
 
 extern "C" cyclus::Agent* ConstructReactorFacility(cyclus::Context* ctx) {
