d5eed6797a8b572757f3db72e5f19da178b523ff
flanflanagan <flanny65@gmail.com>
2015-02-19 13:56:48 -0600

    added power tracking in cymetric

--- src/reactor_facility.cc
+++ src/reactor_facility.cc
@@ -273,7 +273,7 @@ void ReactorFacility::Tock() {
 
   if(shutdown != true && record == true){
       std::cout << "BURNUP: " << fuel_library_.batch[0].discharge_BU << std::endl;
-      //cyclus::toolkit::RecordTimeSeries<cyclus::toolkit::POWER>(a, 42.0);
+      cyclus::toolkit::RecordTimeSeries<cyclus::toolkit::POWER>(this, generated_power);
       //add batch variable to cyclus database
       ///time may need to be fixed by adding cycle length to it
       context()->NewDatum("BrightLite_Reactor_Data")
